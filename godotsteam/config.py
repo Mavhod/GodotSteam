f0cc27d2c94c025f09e40578ab1d41b35a791b8c
Kermer <kermer.games@gmail.com>
2016-01-13 06:31:40 +0100

    Resolves Mavhod/#1

--- godotsteam/config.py
+++ godotsteam/config.py
@@ -17,17 +17,30 @@ def configure(env):
 			env.Append(RPATH=env.Literal('\\$$ORIGIN/linux64'))
 			env.Append(LIBPATH=["#modules/godotsteam/sdk/redistributable_bin/linux64"])
 	elif env["platform"] == "windows":
-		if env["bits"]=="32":
-			#env.Append(LIBS=["steam_api"])
-			env.Append(LINKFLAGS=["steam_api.lib"])
-			env.Append(LIBPATH=["#modules/godotsteam/sdk/redistributable_bin"])
-		elif env["bits"]=="64":
-			# use 64bit steam_api?
-			if (True): # Change to False if you have problems with "unresolved external symbols"
-				env.Append(LINKFLAGS=["steam_api64.lib"])
-				env.Append(LIBPATH=["#modules/godotsteam/sdk/redistributable_bin/win64"])
-			else:
-				env.Append(LINKFLAGS=["steam_api.lib"])
-				env.Append(LIBPATH=["#modules/godotsteam/sdk/redistributable_bin"])
+		steamlib = "steam_api"
+		steamlib_path = "#modules/godotsteam/sdk/redistributable_bin"
+		# use 64bit steam_api on Windows 64bit?
+		use_64bit_lib = True # Change to False if you have problems with "unresolved external symbols"
+		
+		if env["CC"] == "cl": # mostly VisualStudio
+			if env["bits"] == "32":
+				steamlib += ".lib" # steam_api.lib
+			elif env["bits"] == "64":
+				if (use_64bit_lib):
+					steamlib += "64" # steam_api64 (+.lib)
+					steamlib_path += "/win64" # .../sdk/redistributable_bin/win64
+				steamlib += ".lib"
+			env.Append(LINKFLAGS=[ steamlib ])
+		else: # mostly "gcc"
+			# 32bit - stays "steam_api"
+			if env["bits"] == "64":
+				if (use_64bit_lib):
+					steamlib += "64" # steam_api64
+					steamlib_path += "/win64" # .../sdk/redistributable_bin/win64
+				else: # use 32bit
+					steamlib += ".lib" # steam_api.lib
+			env.Append(LIBS=[ steamlib ])
+		
+		env.Append(LIBPATH=[ steamlib_path ])
 
 
